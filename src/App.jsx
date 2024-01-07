87a2bfed13f9e96246e415650c6c772b83ff7b02
anthrofax <afridhoikhsan@gmail.com>
2024-01-07 10:42:57 +0700

    Setting up Pages, Routes, App Layout, Sidebar, & Main Navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard.jsx";
+import AppLayout from "./ui/AppLayout.jsx";
+import PageNotFound from "./pages/PageNotFound.jsx";
+import Login from "./pages/Login.jsx";
+import Account from "./pages/Account.jsx";
+import Settings from "./pages/Settings.jsx";
+import Users from "./pages/Users.jsx";
+import Cabins from "./pages/Cabins.jsx";
+import Bookings from "./pages/Bookings.jsx";
+import {Navigate} from 'react-router-dom'
+
 function App() {
   return (
-    <div>
-      Hello Vite!
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppLayout />}>
+          <Route index element={<Navigate replace to="dashboard"/>}></Route>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="bookings" element={<Bookings />}></Route>
+          <Route path="cabins" element={<Cabins />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="settings" element={<Settings />}></Route>
+          <Route path="account" element={<Account />}></Route>
+        </Route>
+        <Route path="login" element={<Login />}></Route>
+        <Route path="*" element={<PageNotFound />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
