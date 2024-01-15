6d157a352ea6470ece41866f04f1fc93016f97af
anthrofax <afridhoikhsan@gmail.com>
2024-01-15 17:31:17 +0700

    Set up User Account Sign Up page, also implement form input validation

--- src/features/authentication/SignupForm.jsx
+++ src/features/authentication/SignupForm.jsx
@@ -2,26 +2,67 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
+import { useForm } from "react-hook-form";
 
 // Email regex: /\S+@\S+\.\S+/
 
 function SignupForm() {
+  const { register, handleSubmit, formState, getValues } = useForm();
+  const { errors } = formState;
+
+  function onSubmit(submittedData) {
+    console.log(submittedData);
+  }
+
   return (
-    <Form>
-      <FormRow label="Full name" error={""}>
-        <Input type="text" id="fullName" />
+    <Form onSubmit={handleSubmit(onSubmit)}>
+      <FormRow label="Full name" error={errors?.fullName?.message}>
+        <Input
+          type="text"
+          id="fullName"
+          {...register("fullName", { required: "Kolom ini perlu diisi" })}
+        />
       </FormRow>
 
-      <FormRow label="Email address" error={""}>
-        <Input type="email" id="email" />
+      <FormRow label="Email address" error={errors?.email?.message}>
+        <Input
+          type="email"
+          id="email"
+          {...register("email", {
+            required: "Kolom ini perlu diisi",
+            pattern: "/S+@S+.S+/",
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Password (min 8 characters)" error={""}>
-        <Input type="password" id="password" />
+      <FormRow
+        label="Password (min 8 characters)"
+        error={errors?.password?.message}
+      >
+        <Input
+          type="password"
+          id="password"
+          {...register("password", {
+            required: "Kolom ini perlu diisi",
+            minLength: {
+              value: 8,
+              message: "Kata sandi minimal harus terdiri dari 8 karakter ",
+            },
+          })}
+        />
       </FormRow>
 
-      <FormRow label="Repeat password" error={""}>
-        <Input type="password" id="passwordConfirm" />
+      <FormRow label="Repeat password" error={errors?.passwordConfirm?.message}>
+        <Input
+          type="password"
+          id="passwordConfirm"
+          {...register("passwordConfirm", {
+            required: "Kolom ini perlu diisi",
+            validate: (value) =>
+              value === getValues().password ||
+              "Kata sandi yang anda masukkan tidak cocok ",
+          })}
+        />
       </FormRow>
 
       <FormRow>
