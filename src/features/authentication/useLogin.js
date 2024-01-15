f5c58efbc6424de3d83d254cee622e93dbc90162
anthrofax <afridhoikhsan@gmail.com>
2024-01-15 16:46:43 +0700

    Fixing an Important Bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,16 +1,16 @@
-import { useMutation, useQueryClient} from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login } from "../../services/apiAuth";
 import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export function useLogin() {
   const navigate = useNavigate();
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
   const { mutate, isLoading } = useMutation({
     mutationFn: login,
     onSuccess: (user) => {
-      queryClient.setQueriesData(['user'], user);
+      queryClient.setQueryData(["user"], user.user);
       toast.success("Anda berhasil login");
 
       navigate("/dashboard");
