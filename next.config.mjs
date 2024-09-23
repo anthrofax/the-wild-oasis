3a3f45c8b121f09196af99b8bf13efee258a89e4
anthrofax <afridhoikhsan@gmail.com>
2024-09-23 23:12:53 +0700

    Client & Server Interactions

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "atkzebbcihfbeturprwu.supabase.co",
+        hostname: "dclaevazetcjjkrzczpc.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
