bd83f8d317a77dfa3b30a60b3add19cee21d96a3
anthrofax <afridhoikhsan@gmail.com>
2024-10-07 13:05:46 +0700

    Update next.config image pattern

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "dclaevazetcjjkrzczpc.supabase.co",
+        hostname: "atkzebbcihfbeturprwu.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
